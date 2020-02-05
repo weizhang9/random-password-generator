db9afe750883470ba036096a6ed73cbd0ac8c7e8
weizhang9 <wei.zhang@sky.uk>
2020-02-05 18:18:44 +0000

    github pages only allows static content ffs

--- main.go
+++ main.go
@@ -2,21 +2,16 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"math/rand"
-	"net/http"
 	"time"
 )
 
-const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 
+const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
 	fmt.Println(stringWithCharset(32, charset))
-	http.HandleFunc("/", HomePageHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func stringWithCharset(length int, charset string) string {
@@ -25,27 +20,4 @@ func stringWithCharset(length int, charset string) string {
 		b[i] = charset[seededRand.Intn(len(charset))]
 	}
 	return string(b)
-}
-
-type PageVariables struct {
-	Date string
-	Time string
-}
-
-func HomePageHandler(w http.ResponseWriter, r *http.Request) {
-
-	now := time.Now()              
-	HomePageVars := PageVariables{ 
-		Date: now.Format("02-01-2006"),
-		Time: now.Format("15:04:05"),
-	}
-
-	t, err := template.ParseFiles("index.html")
-	if err != nil {                                
-		log.Print("template parsing error: ", err) 
-	}
-	err = t.Execute(w, HomePageVars) 
-	if err != nil {                  
-		log.Print("template executing error: ", err)
-	}
 }
