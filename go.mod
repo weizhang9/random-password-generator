117e949ff3ba02101b1483fdd3ea6fb2c71fe5a7
weizhang9 <wei.zhang@sky.uk>
2020-02-05 19:50:31 +0000

    changed it to use rust instead

--- go.mod
+++
@@ -1,3 +0,0 @@
-module password-generator
-
-go 1.12
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+extern crate rand;
+use std::io;
+
+fn get_len() -> u8 {
+    loop {
+        println!("Input desired password length:");
+    
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("couldn't understand stdin");
+    
+        match guess.trim().parse::<u8>() {
+            Ok(v) => return v,
+            Err(e) => println!("Couldn't understand input {}", e),
+        };
+    }
+}
+
+fn generate(len: u8) -> String {
+    use rand::Rng;
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                            0123456789!@$%^&*()~#";
+    let mut rng = rand::thread_rng();
+
+    let password: String = (0..len)
+        .map(|_| {
+            let idx = rng.gen_range(0, CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect();
+    return password;
+}
+
+fn is_generated(out: usize, input: u8) -> bool {
+    let o : u8 = out as u8;
+    return o == input;
+}
+
+fn main() {
+    loop {
+        let password_len: u8 = get_len();
+        let password: String = generate(password_len);
+        println!("{:?}", password);
+        if is_generated(password.chars().count(), password_len) {
+            break;
+        }
+    }
+}
